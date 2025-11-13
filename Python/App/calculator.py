d0673c813470af44982806251cb97d34851efb8c
senthamil-Devops <senthamil2691@gmail.com>
2025-11-13 20:52:29 +0530

    Python codes

---
+++ Python/App/calculator.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def main():
+    print("Addition:", add(2, 3))
+    print("Subtraction:", subtract(5, 2))
---
+++ Python/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_python_app",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies if needed
+    entry_points={
+        "console_scripts": [
+            "calc=app.calculator:main",
+        ],
+    },
+)
